267727915ffb51fbe3a6d7d0554b2d847edf0223
Luiz Bellumat <lpbs07@gmail.com>
2022-07-14 21:18:20 -0300

    oi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './componentes/layout/layout.component';
+import { HeaderComponent } from './componentes/layout/header/header.component';
+// import {
+//   RiAncientGateFill,
+//   RiHome2Fill,
+//   RiAlignJustify,
+//   RemixIconModule,
+//   RiNotification2Line,
+//   RiUserLine,
+//   RiLightbulbLine,
+//   RiBriefcaseLine,
+//   RiFileTextLine,
+//   RiArchiveDrawerLine,
+//   RiHandCoinLine
+// } from 'angular-remix-icon';
+import { SidebarComponent } from './componentes/layout/sidebar/sidebar.component';
+
+// const icons = {
+//   RiAncientGateFill,
+//   RiHome2Fill,
+//   RiAlignJustify,
+//   RiUserLine,
+//   RiNotification2Line,
+//   RiLightbulbLine,
+//   RiBriefcaseLine,
+//   RiFileTextLine,
+//   RiArchiveDrawerLine,
+//   RiHandCoinLine
+// };
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //RemixIconModule.configure(icons),
   ],
   providers: [],
   bootstrap: [AppComponent]
